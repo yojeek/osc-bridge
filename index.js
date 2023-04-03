2885d499a7a20e1325a889d506fc73adf610645c
msamoylov <antiplaka@gmail.com>
2023-04-03 17:02:11 +0700

    every pose in it's own channel

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ osc.open() // listening on 'ws://localhost:8080'
 
 const socket = dgram.createSocket('udp4')
 
+/*
 osc.on('*', message => {
     console.log(message.args)
     const msg = new OSC.Message(message.address, ...message.args)
     const binary = msg.pack()
     //socket.send(new Buffer(binary), 0, binary.byteLength, 7331, '169.254.235.240')
-    socket.send(new Buffer(binary), 0, binary.byteLength, 7331, '127.20.15.5')
+    socket.send(new Buffer(binary), 0, binary.byteLength, 1234, '127.0.0.1')
+});
+*/
+
+osc.on('*', message => {
+    console.log(message.args)
+    const [_unused, idx, x, y, name] = message.args
+    let binary = (new OSC.Message(message.address + '_' + name + '_x', x)).pack()
+    socket.send(new Buffer(binary), 0, binary.byteLength, 1234, 'localhost')
+    binary = (new OSC.Message(message.address + '_' + name + '_y', y)).pack()
+    socket.send(new Buffer(binary), 0, binary.byteLength, 1234, 'localhost')
 });
 
 
